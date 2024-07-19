e4ead27c47062e2914972853956d5fa7f53f908a
Vladimir <v.ladimirbudilov@yandex.ru>
2024-07-19 23:04:52 +0400

    Add tests for creating employee

---
+++ Application.Tests/AddEmployeeCommandTests.cs
@@ -0,0 +1,45 @@
+﻿using Application.Commands;
+using AutoFixture;
+using Domain.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace Application.Tests;
+
+public class AddEmployeeCommandTests
+{
+    private readonly CommandFixture _fixture= new();
+
+    [Fact]
+    public async Task AddEmployeeCommandHandler_ShouldReturnSuccessResult()
+    {
+        // Arrange
+        var command = new Fixture().Create<AddEmployeeCommand>();
+        var validId = 1;
+        _fixture.employeeRepositoryMock.Setup(x => x.AddAsync(It.IsAny<Employee>())).ReturnsAsync(validId);
+        
+        // Act
+        var result = await _fixture.SendAsync(command);
+        
+        // Assert
+        result.Success.Should().BeTrue();
+        _fixture.employeeRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Employee>()), Times.Once);
+    }
+
+
+    [Fact]
+    public async Task AddEmployeeCommandHandler_ShouldReturnFailResult()
+    {
+        // Arrange
+        var command = new Fixture().Create<AddEmployeeCommand>();
+        var invalidId = -1;
+        _fixture.employeeRepositoryMock.Setup(x => x.AddAsync(It.IsAny<Employee>())).ReturnsAsync(invalidId);
+
+        // Act
+        var result = await _fixture.SendAsync(command);
+
+        // Assert
+        result.Success.Should().BeFalse();
+        _fixture.employeeRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Employee>()), Times.Once);
+    }
+}
---
+++ Application.Tests/CommandFixture.cs
@@ -0,0 +1,32 @@
+﻿using Application.DI;
+using Domain.Repositories;
+using Infrastructure.DI;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+
+namespace Application.Tests;
+
+public class CommandFixture
+{
+    public Mock<IEmployeeRepository> employeeRepositoryMock;
+    private IMediator _mediator;
+    
+    public CommandFixture()
+    {
+        employeeRepositoryMock = new Mock<IEmployeeRepository>();
+        var services = new ServiceCollection();
+        services.AddMediatR(opt=> 
+            opt.RegisterServicesFromAssembly(typeof(ApplicationServiceRegistration).Assembly));
+        services.AddSingleton(employeeRepositoryMock.Object);
+        var serviceProvider = services.BuildServiceProvider();
+        
+        _mediator = serviceProvider.GetRequiredService<IMediator>();
+    }
+    
+    public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+    {
+        return await _mediator.Send(request);
+    }
+}   
